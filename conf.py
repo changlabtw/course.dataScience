30419820143f4f4af69287420e65823c5a85f534
Jia-Ming Chang <chang.jiaming@gmail.com>
2023-10-09 19:56:49 +0800

    debug an error when using html_theme = "sphinx_rtd_theme"

--- conf.py
+++ conf.py
@@ -31,8 +31,9 @@ release = 'v1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  ...
+  'sphinx_rtd_theme',
 ]
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -53,3 +54,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_path = ["_themes", ]
